8967974e8f747fcd9c84c142423e09ffa307e2e9
Jamila Otieno <jamila.otieno@duke.edu>
2020-10-16 23:23:56 -0400

    clean up repo and modify App.js component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App/App.js';
+import * as serviceWorker from './setup/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
